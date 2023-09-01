fb7798745a340f2969d8d19f2b8d351ed3b2f60d
Lenaic-ouendo-01 <malloryouendo@gmail.com>
2023-09-01 14:00:19 +0200

    Application of Intersection Observer to make animation

--- main.js
+++ main.js
@@ -1,24 +1,82 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const observer = new IntersectionObserver((entries) => {
+  for(const entry of entries) {
+    // console.log(
+         //Pour récupérer un élément
+    //   entry.target,
+         //Pour suivre la visibilité d'un élément il faudra utiliser le isIntersecting 
+    //   entry.isIntersecting
+    // )
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    //Si l'élément (isIntersecting) est à true alors on appliquera l'animation définie
+    if(entry.isIntersecting) {
+      entry.target.animate([
+        {transform: 'translateY(50%)', opacity: 0},
+        {transform: 'translateY(0%)', opacity: 1},
+      ], {
+        duration: 1000
+      })
 
-setupCounter(document.querySelector('#counter'))
+      //Pour déclancher l'élément qu'une seule fois (arrêter l'observation et tout ce qui s'y applique comme animation)
+      observer.unobserve(entry.target)
+    }
+
+  }
+},{
+  //Pour élagir la porté du ViewPort. C'est en quelque sorte pour dire que l'élément sera visible lorsqu'on sera dans la zone du Viewport définit  
+  //rootMargin: '50px 50px 50px 50px'
+
+  //Pour spécifier qu'il faut qu'au moins 50% de l'élément soit visible avant de l'afficher
+  // threshold: .1
+})
+
+const observerX = new IntersectionObserver((entries) => {
+  for(const entry of entries) {
+
+    if(entry.isIntersecting) {
+      entry.target.animate([
+        {transform: 'translateX(-100%)', opacity: 0},
+        {transform: 'translateX(0%)', opacity: 1},
+      ], {
+        duration: 1000
+      })
+      observerX.unobserve(entry.target)
+    }
+
+  }
+},{
+    // threshold: .1 
+})
+
+const observerC = new IntersectionObserver((entries) => {
+  for(const entry of entries) {
+
+    if(entry.isIntersecting) {
+      entry.target.animate([
+        {transform: 'scale(0)', opacity: 0},
+        {transform: 'scale(1)', opacity: 1},
+      ], {
+        duration: 1000
+      })
+      observerC.unobserve(entry.target)
+    }
+
+  }
+},{
+    // threshold: .1 
+})
+
+observer.observe(document.querySelector('.vu-1'))
+observer.observe(document.querySelector('.vu-2'))
+observer.observe(document.querySelector('.vu-3'))
+observer.observe(document.querySelector('.vu-4'))
+observer.observe(document.querySelector('.vu-5'))
+observer.observe(document.querySelector('.vu-6'))
+observer.observe(document.querySelector('.vu-7'))
+observer.observe(document.querySelector('.vu-8'))
+observer.observe(document.querySelector('.vu-9'))
+observerX.observe(document.querySelector('.vuX-1'))
+observerX.observe(document.querySelector('.vuX-2'))
+observerC.observe(document.querySelector('.vuC-1'))
+
+//Pour déconnecter tous les observers
+// observer.disconnect()
